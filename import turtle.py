043f26dae24ec8bd03b76e2881f22b7644531af1
Omid <153293580+Omid2831@users.noreply.github.com>
2024-10-26 15:17:23 +0200

    Draw Time

---
+++ import turtle.py
@@ -0,0 +1,44 @@
+import turtle
+qazi_turtle = turtle.Screen()
+qazi_turtle = qazi_turtle.bgcolor('black')
+qazi_turtle = turtle.Turtle()
+qazi_turtle.end_poly()
+qazi_turtle.pensize(20)
+
+qazi_turtle.speed(1)
+
+qazi_turtle.pencolor('yellow')
+
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+
+qazi_turtle.forward(100)
+qazi_turtle.left(90)
+
+qazi_turtle.forward(100)
+qazi_turtle.left(90)
+
+qazi_turtle.forward(100)
+qazi_turtle.left(90)
+
+qazi_turtle.forward(100)
+
+qazi_turtle.left(90)
+qazi_turtle.forward(100)
+
+qazi_turtle.pencolor('red')
+
+qazi_turtle.circle(360)
+qazi_turtle.circle(-360)
+
+qazi_turtle.pencolor('blue')
+
+qazi_turtle.left(45)
+qazi_turtle.forward(100)
+xx = input()
